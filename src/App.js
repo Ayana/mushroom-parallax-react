ca7f3f393711e5fa44f78c2023fbf6355ba9b998
Ayana Osawa <ayana.osawa@gmail.com>
2020-09-27 17:40:05 -0400

    Add basic parallax

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import "./assets/styles/App.scss"
 
 function App() {
@@ -16,7 +16,6 @@ function App() {
           // get correct height
           const vh = window.innerHeight * 0.01
           document.documentElement.style.setProperty("--vh", `${vh}px`)
-          console.log("invoke")
         }
       }, 500)
     }
@@ -25,16 +24,74 @@ function App() {
     return () => {
       window.removeEventListener("resize", resizeListener)
     }
-  }, [])
+  })
+
+  // const [x, setX] = useState("null")
+  // const [y, setY] = useState("null")
+  useEffect(() => {
+    const button = document.querySelector(".button")
+    const container = document.querySelector(".parallax-container")
+    const layer1 = document.querySelector(".layer1")
+    const layer2 = document.querySelector(".layer2")
+    const layer3 = document.querySelector(".layer3")
+    const layer4 = document.querySelector(".layer4")
+    const layer5 = document.querySelector(".layer5")
+    const layer6 = document.querySelector(".layer6")
+    const layer7 = document.querySelector(".layer7")
+    button.addEventListener("click", onClick)
+
+    function onClick() {
+      // feature detect
+      if (typeof DeviceOrientationEvent.requestPermission === "function") {
+        DeviceOrientationEvent.requestPermission()
+          .then((permissionState) => {
+            if (permissionState === "granted") {
+              button.style.display = "none"
+              window.addEventListener("deviceorientation", (e) => {
+                const x = e.gamma
+                const y = e.beta
+                layer2.style.transform = `translate3d(${-x}px,${-y}px,${x + y}px`
+              })
+            }
+          })
+          .catch(console.error)
+      } else {
+        button.style.display = "none"
+        container.style.display = "block"
+        // handle regular non iOS 13+ devices
+        document.body.addEventListener("mousemove", (e) => {
+          const x = e.clientX
+          const y = e.clientY
+          // setX(x)
+          // setY(y)
+          layer1.style.transform = `translate3d(${-x * 0.3}px,${-y * 0.08}px,${x + y}px`
+          layer2.style.transform = `translate3d(${-x * 0.18}px,${-y * 0.06}px,${x + y}px`
+          layer3.style.transform = `translate3d(${-x * 0.1}px,${-y * 0.05}px,${x + y}px`
+          layer4.style.transform = `translate3d(${-x * 0.08}px,${-y * 0.04}px,${x + y}px`
+          layer5.style.transform = `translate3d(${-x * 0.09}px,${-y * 0.03}px,${x + y}px`
+          layer6.style.transform = `translate3d(${-x * 0.04}px,${-y * 0.02}px,${x + y}px`
+          layer7.style.transform = `translate3d(${-x * 0.03}px,${-y * 0.01}px,${x + y}px`
+        })
+      }
+    }
+  })
 
   return (
-    <div className="parallax-container">
-      <div className="parallax-layer layer5"></div>
-      <div className="parallax-layer layer4"></div>
-      <div className="parallax-layer layer3"></div>
-      <div className="parallax-layer layer2"></div>
-      <div className="parallax-layer layer1"></div>
-    </div>
+    <>
+      <button className="button">CLICK TO SHOW</button>
+      {/* <div className="note">
+        {x} / {y}
+      </div> */}
+      <div className="parallax-container">
+        <div className="parallax-layer layer7"></div>
+        <div className="parallax-layer layer6"></div>
+        <div className="parallax-layer layer5"></div>
+        <div className="parallax-layer layer4"></div>
+        <div className="parallax-layer layer3"></div>
+        <div className="parallax-layer layer2"></div>
+        <div className="parallax-layer layer1"></div>
+      </div>
+    </>
   )
 }
 
