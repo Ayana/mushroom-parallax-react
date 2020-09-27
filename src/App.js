27d6b943355d398917c7ff2131c38c7fb8a963a0
Ayana Osawa <ayana.osawa@gmail.com>
2020-09-27 16:07:18 -0400

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react"
+import "./assets/styles/App.scss"
 
 function App() {
+  useEffect(() => {
+    // Get correct height function with clearTimeout to prevent too many requests
+    const vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty("--vh", `${vh}px`)
+    const currentWidth = window.innerWidth
+
+    let timeoutId = null
+    const resizeListener = () => {
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => {
+        if (currentWidth !== window.innerWidth) {
+          // get correct height
+          const vh = window.innerHeight * 0.01
+          document.documentElement.style.setProperty("--vh", `${vh}px`)
+          console.log("invoke")
+        }
+      }, 500)
+    }
+    window.addEventListener("resize", resizeListener)
+
+    return () => {
+      window.removeEventListener("resize", resizeListener)
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parallax-container">
+      <div className="parallax-layer layer5"></div>
+      <div className="parallax-layer layer4"></div>
+      <div className="parallax-layer layer3"></div>
+      <div className="parallax-layer layer2"></div>
+      <div className="parallax-layer layer1"></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
