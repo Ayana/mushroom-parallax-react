fd85fade97554a257282f783e35c918bac214d70
Ayana Osawa <ayana.osawa@gmail.com>
2020-09-28 17:33:58 -0400

    Add viewport

--- src/Components/Helmet.js
+++ src/Components/Helmet.js
@@ -6,6 +6,7 @@ export default () => {
     <div className="application">
       <Helmet>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </Helmet>
     </div>
   )
